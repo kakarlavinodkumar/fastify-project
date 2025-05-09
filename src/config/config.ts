da93fcf8156c3e7bd8921f21ea082804975410fa
vinod kumar kakarla <vinodkakarla564@gmail.com>
2025-05-09 15:58:44 -0700

    feat: add configuration setup with dotenv for environment variables

---
+++ src/config/config.ts
@@ -0,0 +1,7 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const config = {
+    AZURE_BUS_CONNECTION_STRING :  process.env.AZURE_BUS_CONNECTION_STRING,
+    PORT: Number(process.env.PORT)
+}
