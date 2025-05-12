8240cafc0019f62ac63d5cd45a8bf907c052d273
vinod kumar kakarla <vinodkakarla564@gmail.com>
2025-05-12 16:59:21 -0700

    feat: improve plugin registration comments for better clarity in index.ts

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,5 +1,12 @@
 import { FastifyInstance } from 'fastify';
+import { setupDB } from '../database/connect';
+import { setupRoutes } from '../server/route';
 
+// Register plugins here
 export async function registerPlugins(fastify: FastifyInstance) {
-    // Register your plugins here
+    // Register database connection
+    await fastify.register(setupDB);
+
+    // Register routes
+    await fastify.register(setupRoutes, { prefix: '/api/v1/mercury' });    
 }
