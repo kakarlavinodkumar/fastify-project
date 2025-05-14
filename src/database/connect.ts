64db2169f3d487cf0fc319d38cc96b2c8788b791
vinod kumar kakarla <vinodkakarla564@gmail.com>
2025-05-14 15:44:57 -0700

    refactor: replace pg client connection with TypeORM plugin for database integration

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,22 +1,24 @@
 import fastifyPlugin from 'fastify-plugin';
 import { FastifyInstance } from 'fastify';
-import { Client } from 'pg';
+import fastifyORMPlugin from 'typeorm-fastify-plugin';
 
-// postgres://postgres:postgres@localhost:5432/postgres
+import { Message } from './entities/message';
+
 async function dbConnector(fastify: FastifyInstance) {
-    const client = new Client({
-        connectionString: 'postgres://postgres:postgres@localhost:5432/postgres',
-    });
-
     try {
-        await client.connect();
-        fastify.decorate('pg', client);
-
-        fastify.log.info('Database connected successfully');
-        fastify.addHook('onClose', (instance, done) => {
-            client.end().then(() => done()).catch(done);
-        });
-        
+        fastify.register(fastifyORMPlugin, {
+            host: 'localhost',
+            port: 5432,
+            type: 'postgres',
+            database: 'postgres',
+            username: 'postgres',
+            password: 'postgres',
+            synchronize: true, // Automatically sync schema (use cautiously in production)
+            logging: true,
+            entities: [Message], // Register the entities here
+        }).ready();
+      
+        await fastify.after();
     } catch (err) {
         fastify.log.error(err);
         throw err;
