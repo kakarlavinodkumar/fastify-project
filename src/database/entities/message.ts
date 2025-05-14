86712ac5f5e199526965d92bb9867517808a5739
vinod kumar kakarla <vinodkakarla564@gmail.com>
2025-05-14 14:03:33 -0700

    feat: add Message entity with flightInstanceId, flightNumber, type, status, channel, time, and message fields

---
+++ src/database/entities/message.ts
@@ -0,0 +1,37 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('message')
+export class Message {
+    @PrimaryGeneratedColumn()
+    Id!: number;
+
+    @Column()
+    flightInstanceId!: string;
+
+    @Column()
+    flightNumber!: string;
+
+    @Column({
+        type: 'enum',
+        enum: ['Automated', 'Ad-hoc'],
+    })
+    type!: 'Automated' | 'Ad-hoc';
+
+    @Column({
+        type: 'enum',
+        enum: ['Sent', 'Scheduled', 'Paused'],
+    })
+    status!: 'Sent' | 'Scheduled' | 'Paused';
+
+    @Column({
+        type: 'enum',
+        enum: ['sms', 'email'],
+    })
+    channel!: 'sms' | 'email';
+
+    @Column()
+    time!: Date;
+
+    @Column()
+    message!: string
+}
